728efcdf3f1777c39603f931a80785934448b22f
asurazur <asurazur@gmail.com>
2024-03-05 02:07:57 +0800

    Implemented Update Function

--- contacts.sh
+++ contacts.sh
@@ -6,38 +6,46 @@
 # scriptname [option] [argument] ...
 
 # Function to get the index of a person by id
-function map_id_to_index() {
-    local id=$1
+map_id_to_index() {
+    local id="$1"
+    local ids=($(jq -r '.people[].id' "db.json"))
     local index=0
-    local found=false
-
-    # Read the JSON file and extract the ids
-    ids=$(jq '.people[].id' db.json)
-
-    # Iterate over the extracted ids
-    for current_id in $ids; do
-        # Check if the current id matches the input id
-        if [ "$current_id" -eq "$id" ]; then
-            # Set found to true and break the loop
-            found=true
-            break
+    for i in "${ids[@]}"; do
+        # ID found
+        if [[ $i == $id ]]; then
+            echo "$index"
+            return
         fi
-        # Increment the index counter
-        ((index++))
+        index=$((index + 1))
     done
-
-    # If the id is found, return its index, otherwise return -1
-    if [ "$found" = true ]; then
-        echo $index
-    else
-        echo -1
-    fi
+    # If the id is not found
+    echo "-1"
+    {
+      "id": "3",
+      "name": "test",
+      "gender": "test",
+      "birthday": "test",
+      "location": "M",
+      "email": "2002-09-09",
+      "number": "Naga",
+      "motto": "City,"
+    },
+    {
+      "id": "4",
+      "name": "KD",
+      "gender": "test",
+      "birthday": "s",
+      "location": "d",
+      "email": "d",
+      "number": "d",
+      "motto": "d"
+    }
 }
 
 function create_unique_id() {
-    ids=$(jq '.people[].id' db.json)
+    local ids=($(jq -r '.people[].id' "db.json"))
     local largest=-1
-    for i in $ids; do
+    for i in "${ids[@]}"; do
         if [[ $largest -lt $i ]]; then
             largest=$i
         fi
@@ -74,51 +82,102 @@ function add_person_to_json() {
     echo "New person added with ID: $id"
 }
 
+function get_attribute() {
+    if [[ $# != 1 ]]; then
+        echo invalid number of arguments
+    else
+        if [[ $1 == 1 ]];then 
+            echo name
+        elif [[ $1 == 2 ]];then 
+            echo email
+        elif [[ $1 == 3 ]];then 
+            echo number
+        elif [[ $1 == 4 ]];then 
+            echo birthday
+        elif [[ $1 == 5 ]];then 
+            echo gender
+        elif [[ $1 == 6 ]];then 
+            echo location
+        elif [[ $1 == 7 ]];then 
+            echo motto
+        else
+            echo invalid argument
+        fi
+    fi
+}
 #Main Function
 
 #Initialization
 json=$(cat db.json)
 people_size=$(echo $json | jq -r '.people | length')
-echo $json | jq -r '.people[]'
+echo Contact Management Program
 read -p "1:Create, 2:Read, 3:Update, 4:Delete: " -n 1 action; clear
-echo $json | jq -r '.people[]';clear
+
 
 #Create/Add Contact
 if [[ $action == 1 ]]; then
-    echo "Create"
+    echo "Add Contact"
     #Create Unique ID
     id=$(create_unique_id)
     #Prompt for Credentials
-    read -p "Please Enter Your Name: " name
-    read -p "Hello $name, What's Your Gender Identity? " gender
-    read -p "When's Your Birthday (YYYY-MM-DD)? " birthday
-    read -p "Where are you from? " location
-    read -p "What's your email? " email
-    read -p "How about your phone number: " number
-    read -p "Phewwww, Lastly, What's your Motto? " motto
+    read -p "Please Enter Your Name: " -r  name
+    read -p "Hello $name, What's Your Gender Identity? " -r gender
+    read -p "When's Your Birthday (YYYY-MM-DD)? " -r birthday
+    read -p "Where are you from? " -r location
+    read -p "What's your email? " -r email
+    read -p "How about your phone number: " -r number
+    read -p "Phewwww, Lastly, What's your Motto? " -r motto
     echo Thank You For Your Cooperation
     #Write to JSON FILE
     add_person_to_json $id $name $gender $birthday $location $email $number $motto
 
 #Read Contact
 elif [[ $action == 2 ]]; then
-    echo "Read"
-    read -p "Enter ID to read: " id
-    #Map ID to Index
-    actual_index=$(map_id_to_index "$id")]
-    #Check if ID exists
-    if [[ $actual_index == "-1" ]]; then
-        echo "$id not found"
+    echo "Read Contact"
+    read -p "Option (0-All Contacts, 1-Single Contact): " -n 1 option; echo
+    if [[ $option == 0 ]]; then
+        echo $json | jq -r '.people[]';
+    elif [[ $option == 1 ]]; then
+        read -p "Enter ID to read: " id;
+        #Map ID to Index
+        actual_index=$(map_id_to_index "$id")
+        #Check if ID exists
+        if [[ $actual_index == "-1" ]]; then
+            echo "$id not found"
+        else 
+            echo $json | jq -r ".people[$actual_index]"
+        fi
     else 
-        echo $json | jq -r ".people[$actual_index]"
+        echo "Invalid Argument"
     fi
 
 #Update Contact    
 elif [[ $action == 3 ]]; then
     echo "Update"
-    #PROMPT ID
-    #UPDATE DATA
-    #CONFIRMATION
+    #Map ID to Index
+    read -p "Enter ID to update: " id
+    actual_index=$(map_id_to_index "$id")
+    #Check if ID exists
+    if [[ $actual_index == -1 ]]; then
+        echo $id not found
+    else
+        #UPDATE DATA
+        echo $json | jq -r ".people[$actual_index]"
+        echo "Updating Instruction: (1: Name, 2: Email, 3: Number, 4: Birthday, 5: Gender, 6: Location, 7: Motto"
+        read -p "Choose Which to Update: " -n 1 option; echo
+        attribute=$(get_attribute "$option")
+        read -p "Enter the New $attribute: " new_attribute
+        old_attribute=$(echo $json | jq ".people[$actual_index].$attribute")
+        #CONFIRMATION
+        read -p "Are you sure to replace $old_attribute with $new_attribute (Y/y to confirm)" flag
+        if [ "$flag" == "y" ] || [ "$flag" == "Y" ]; then
+            echo $json | jq ".people[$actual_index].$attribute=\"$new_attribute\"" > tmp.json
+            mv tmp.json db.json
+            echo "$id.$attribute: $old_attribute -> $new_attribute"
+        else
+            echo "Update Abort"
+        fi
+    fi
 
 #Delete Contact
 elif [[ $action == 4 ]]; then
@@ -132,10 +191,10 @@ elif [[ $action == 4 ]]; then
     else 
         #Delete Confirmation
         echo $json | jq -r ".people[$actual_index]"
-        read -p "Are you sure to delete this (Y:Yes/ N:No): " flag 
+        read -p "Are you sure to delete this (Y/y to confirm): " flag 
         if [ "$flag" == "y" ] || [ "$flag" == "Y" ]; then
-            echo $(jq ".people |= map(select(.id != $id))" db.json) > tmp.json 
-            mv tmp.json db.json
+            echo $json | jq ".people |= map(select(.id != $id))" > tmp.json 
+            # mv tmp.json db.json
             echo "ID: $id Deleted successfully"
         else
             echo "Deletion aborted"
